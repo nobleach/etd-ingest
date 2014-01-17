e0567b49b36860aa0e4d5c00d649bfbf52ef327a
Jim Wharton <jamesawharton@gmail.com>
2014-01-17 07:56:34 -0700

    add logging

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ var sequelize = new Sequelize('rsacfod', 'rsacfod', 'rsacfodadmin')
 var Incident = sequelize.import(__dirname + "/models/Import");
 
 downloader.downloadString("http://fam.nwcg.gov/fam-web/hist_209/get_rsac_209_1", function(report) {
+  console.log(report);
   var allIncidents = arrayBuilder.divideIncidents(report);
 
-  // var incidentsLength = allIncidents.length;
-  // for(var i=0; i<incidentsLength; i++) {
-  //   objectBuilder.buildIncident(allIncidents[i], function(incident) {
-  //     console.log(incident);
-  //   });
-  // }
+  var incidentsLength = allIncidents.length;
+  for(var i=0; i<incidentsLength; i++) {
+    objectBuilder.buildIncident(allIncidents[i], function(incident) {
+      console.log(incident);
+    });
+  }
 
   _.each(allIncidents, function(incident) {
     objectBuilder.buildIncident(incident, function(incidentObj) {
