effc2bfdf813703457f4062680c959237a953409
Jim Wharton <jamesawharton@gmail.com>
2014-02-25 18:50:18 -0700

    add stub for downloader.getUpdates()

--- test/DownloaderSpec.js
+++ test/DownloaderSpec.js
@@ -1,18 +1,38 @@
 var expect = require('chai').expect;
-describe("Downloader Suite", function() {
-  var downloader;
+var sinon = require('sinon');
 
-  beforeEach(function() {
+describe("Downloader Suite", function() {
+  var url, downloader;
+
+  beforeEach(function(done){
     downloader = require('../lib/Downloader.js');
+    url = "https://irwint.doi.gov/arcgis/rest/services/Irwin/MapServer/exts/Irwin/GetUpdates?fromDateTime=2014-02-24T12%3A00%3A00Z&toDateTime=2014-02-25T12%3A00%3A00Z&f=pjson";
+    sinon
+      .stub(downloader, 'getUpdates')
+      .yields(JSON.stringify( {incidents: []} ));
+    done();
   });
 
-  it("given a date range it should download a JSON array from URL", function(done) {
-    var url = "https://irwint.doi.gov/arcgis/rest/services/Irwin/MapServer/exts/Irwin/GetUpdates?fromDateTime=2014-02-24T12%3A00%3A00Z&toDateTime=2014-02-25T12%3A00%3A00Z&f=pjson";
+  afterEach(function(done){
+    downloader.getUpdates.restore();
+    done();
+  });  
+
+  it("should download a JSON object from URL", function(done) {
+    downloader.getUpdates(url, function(report) {
+      var incidents = JSON.parse(report);
+      expect(downloader.getUpdates.called).to.equal(true);
+      expect(report).to.not.be.empty;
+      done();
+    })
+  });
+
+  it('should have a key named "incidents"', function(done) {
     downloader.getUpdates(url, function(report) {
       var incidents = JSON.parse(report);
       expect(incidents).to.be.a('object');
       expect(incidents).to.have.property('incidents');
       done();
-    })
+    });
   });
 });
