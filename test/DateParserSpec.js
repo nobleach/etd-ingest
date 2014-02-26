138736e5aa3086673dfcd00f4975ec8ea0c7b257
Jim Wharton <jamesawharton@gmail.com>
2014-02-26 09:52:13 -0700

    add date methods for required date format

--- test/DateParserSpec.js
+++ test/DateParserSpec.js
@@ -1,4 +1,6 @@
-var expect = require('expect.js');
+var expect = require('chai').expect;
+var moment = require('moment');
+
 describe("DateParser Suite", function() {
   var dateParser;
   // var expect = require('expect');
@@ -8,8 +10,13 @@ describe("DateParser Suite", function() {
     // dateParser = new DateParser();
   });
 
+  it("should take a date and return datetime", function() {
+    var aprilFirst = moment.utc('2013-04-01T12:00:00'); 
+    expect(dateParser.getDateTime(aprilFirst)).to.equal("2013-04-01T12:00:00Z");
+  });
+
   it("should parse D-{MONTH abbreviation}-YY to YYYY-MM-DD", function() {
-    expect(dateParser.parseAsMySqlDate("22-OCT-13")).to.be("2013-10-22");
+    expect(dateParser.parseAsMySqlDate("22-OCT-13")).to.equal("2013-10-22");
   });
   it("should zero fill dates (days and/or months less than 10)", function() {
     expect(dateParser.parseAsMySqlDate("14-MAY-13")).to.equal("2013-05-14");
@@ -18,15 +25,15 @@ describe("DateParser Suite", function() {
     expect(dateParser.parseAsMySqlDate("1-JUN-12")).to.equal("2012-06-01");
   });
   it("should parse M/D/YY to YYYY-MM-DD", function() {
-    expect(dateParser.parseAsMySqlDate("3/10/11")).to.be("2011-03-10");
+    expect(dateParser.parseAsMySqlDate("3/10/11")).to.equal("2011-03-10");
   });
 
   it("should handle leap year dates correctly", function() {
     //leap year
-    expect(dateParser.parseAsMySqlDate("29-FEB-08")).to.be("2008-02-29");
+    expect(dateParser.parseAsMySqlDate("29-FEB-08")).to.equal("2008-02-29");
   });
 
   it("should return correct ISO dates if they are already in that format", function() {
-    expect(dateParser.parseAsMySqlDate("2013-06-07")).to.be("2013-06-07");
+    expect(dateParser.parseAsMySqlDate("2013-06-07")).to.equal("2013-06-07");
   });
 });
